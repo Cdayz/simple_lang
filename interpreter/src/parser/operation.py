c73cfee9572d8b8dd3802eab711030b851fc2c35
cdayz <cdayz@yandex.ru>
2019-07-05 22:15:37 +0300

    feat(parser): Add operation data-structure for parser

---
+++ interpreter/src/parser/operation.py
@@ -0,0 +1,53 @@
+"""Module with type-classes for Parser."""
+
+import enum
+import typing
+import dataclasses
+
+
+class OperationType(enum.Enum):
+    """Enum of different types of operations."""
+
+    Nop = 0
+    Unary = 1
+    Binary = 2
+
+
+class OperationArgumentType(enum.Enum):
+    """Enum of different types of operation arguments."""
+
+    Register = 0
+    RegisterPointer = 1
+    InPlaceValue = 2
+
+
+@dataclasses.dataclass
+class OperationArgument:
+    """Class which represents operation argument.
+
+    :param arg_type: type of Argument
+    :type arg_type: :class:`~.OperationArgumentType`
+
+    :param str arg_word: String representation of argument
+    """
+
+    arg_type: OperationArgumentType
+    arg_word: str
+
+
+@dataclasses.dataclass
+class Operation:
+    """Class which represents operation.
+
+    :param op_type: type of Operation
+    :type op_type: :class:`~.OperationType`
+
+    :param str op_word: String of operation word
+
+    :param op_args: Arguments of operation
+    :type op_args: List[OperationArgument]
+    """
+
+    op_type: OperationType
+    op_word: str
+    op_args: typing.List[OperationArgument]
