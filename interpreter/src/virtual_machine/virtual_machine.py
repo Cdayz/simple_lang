a2f82f9129f0816222019d00457684b65ed3beb6
cdayz <cdayz@yandex.ru>
2019-07-06 23:50:31 +0300

    feat(virtual_machine): Add basic-dummy mov function implementation

--- interpreter/src/virtual_machine/virtual_machine.py
+++ interpreter/src/virtual_machine/virtual_machine.py
@@ -1,8 +1,18 @@
 """Module with virtual machine state and some vm-related functional."""
 
 import io
+import copy
+import struct
 import typing
+import functools
 import dataclasses
+
+from interpreter.src.virtual_machine.bytecode import BYTECODES
+
+VM_OPERATION_TO_BYTECODE = {
+    bytecode: operation
+    for operation, bytecode in BYTECODES.items()
+}
 
 VM_MEM_SIZE = 1024
 VM_OP_SIZE = 12
@@ -27,3 +37,53 @@ class VmState:
     # Code execution
     vm_code_pointer: int = 0
     vm_code_buffer: io.BytesIO = dataclasses.field(default_factory=io.BytesIO)
+
+
+def vm_operation(func):
+    """Decorator around operations on VmState."""
+
+    @functools.wraps(func)
+    def wrapper(vm_state, *args, **kwargs):
+        new_state: VmState = copy.deepcopy(vm_state)
+        operation_bytecode = struct.unpack(
+            '=hbibi',
+            new_state.vm_code_buffer.read1(12)
+        )
+
+        kwargs['op_bytecode'] = operation_bytecode
+
+        new_state = func(new_state, *args, **kwargs)
+
+        new_state.vm_code_pointer += 12
+
+        return new_state
+
+    return wrapper
+
+
+@vm_operation
+def vm_mov(vm_state: VmState, *args, op_bytecode=None) -> VmState:
+    """MOV function for virtual machine."""
+    op_code, arg1_type, arg1, arg2_type, arg2 = op_bytecode
+
+    assert VM_OPERATION_TO_BYTECODE[op_code] == 'MOV'
+
+    # get input
+    if arg2_type == 2:  # Register
+        pass
+    elif arg2_type == 3:  # Register pointer
+        pass
+    elif arg2_type == 4:  # In-place value
+        pass
+    else:
+        raise Exception("Bad argument for mov")
+
+    # Write to output
+    if arg1_type == 2:  # Register
+        pass
+    elif arg1_type == 3:  # RegisterPointer
+        pass
+    else:
+        raise Exception("Bad argument to Mov")
+
+    return vm_state
