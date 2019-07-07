b9d36f987e014f0b2336f5f15df318141b5ae539
cdayz <cdayz@yandex.ru>
2019-07-07 14:04:46 +0300

    feat(virtual_machine): Add VM executor

---
+++ interpreter/src/virtual_machine/vm/vm_executor.py
@@ -0,0 +1,42 @@
+import io
+import struct
+
+from interpreter.src.virtual_machine.vm.vm_def import VmState
+
+from interpreter.src.virtual_machine.vm import VM_BYTECODE_FUNC
+
+
+def initialize_vm(bytecode: io.BytesIO) -> VmState:
+    """Init vm state with given bytecode.
+
+    :param bytecode: Bytecode
+    :type bytecode: io.BytesIO
+
+    :return: Initialized VmState
+    :rtype: VmState
+    """
+    return VmState(
+        vm_code_buffer=bytecode
+    )
+
+
+def execute_bytecode(bytecode: io.BytesIO):
+    """Execute bytecode into Virtual Machine.
+
+    :param bytecode: Bytecode for executing
+    :type bytecode: io.BytesIO
+    """
+    code_size = len(bytecode.read())
+    bytecode.seek(0)
+    vm_state = initialize_vm(bytecode)
+
+    while vm_state.vm_code_pointer < code_size:
+        vm_state.vm_code_buffer.seek(vm_state.vm_code_pointer)
+        bcode = vm_state.vm_code_buffer.read1(2)
+        opcode = struct.unpack('=h', bcode)[0]
+
+        vm_state.vm_code_buffer.seek(
+            vm_state.vm_code_pointer
+        )
+
+        vm_state = VM_BYTECODE_FUNC[opcode](vm_state)
