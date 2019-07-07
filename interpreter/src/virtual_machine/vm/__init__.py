fbdbe9497fa524f377bddbb15c75c07cbb8ef787
cdayz <cdayz@yandex.ru>
2019-07-07 13:14:46 +0300

    feat(virtual_machine): Add export functions for bytecode interpritation

---
+++ interpreter/src/virtual_machine/vm/__init__.py
@@ -0,0 +1,45 @@
+from interpreter.src.virtual_machine.vm.binary_ops import (
+    vm_add,
+    vm_sub,
+    vm_mul,
+    vm_div,
+    vm_and,
+    vm_or,
+    vm_xor,
+    vm_mov,
+    vm_not,
+)
+from interpreter.src.virtual_machine.vm.jumps_and_labels import (
+    vm_nop,
+    vm_cmp,
+    vm_jmp,
+    vm_label,
+    vm_jump_eq,
+    vm_jump_lt,
+    vm_jump_gt,
+    vm_jump_ne,
+)
+from interpreter.src.virtual_machine.vm.io_ops import (
+    vm_input,
+    vm_print
+)
+
+from interpreter.src.virtual_machine.bytecode import BYTECODES
+
+
+FUNCTIONS = (
+    vm_add, vm_sub, vm_div, vm_mul,
+    vm_and, vm_or, vm_xor, vm_not,
+    vm_mov, vm_cmp, vm_jmp, vm_jump_eq,
+    vm_jump_gt, vm_jump_lt, vm_jump_ne,
+    vm_label, vm_print, vm_input, vm_nop
+)
+
+
+VM_BYTECODE_FUNC = {
+    bytecode: func
+    for bytecode, func in zip(
+        [bytecode for _, bytecode in BYTECODES.items()],
+        FUNCTIONS,
+    )
+}
