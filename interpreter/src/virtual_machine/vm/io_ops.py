9bfaa64fcd05f3a98a1238ba579b041791c824be
cdayz <cdayz@yandex.ru>
2019-07-07 13:14:09 +0300

    feat(virtual_machine): Add io operations

---
+++ interpreter/src/virtual_machine/vm/io_ops.py
@@ -0,0 +1,55 @@
+from interpreter.src.virtual_machine.vm.vm_def import (
+    VmState,
+    VM_OPERATION_TO_BYTECODE
+)
+from interpreter.src.virtual_machine.vm.helpers import vm_operation
+
+
+@vm_operation
+def vm_input(vm_state: VmState, *args, op_bytecode=None, **kwargs) -> VmState:
+    """Input value from stdin and write it to memory or register."""
+    op_code, arg1_type, arg1, _, _ = op_bytecode
+
+    assert VM_OPERATION_TO_BYTECODE[op_code] == "INPUT"
+
+    while True:
+        try:
+            input_value = int(input("VM INPUT: "))
+        except ValueError:
+            continue
+        else:
+            break
+
+    if arg1_type == 2:  # Register
+        vm_state.vm_registers[arg1].value = input_value
+
+    elif arg1_type == 3:  # Register pointer
+        mem_address = vm_state.vm_registers[arg1].value
+        vm_state.vm_memory[mem_address] = input_value
+
+    else:
+        raise Exception("Bad input destination")
+
+    return vm_state
+
+
+@vm_operation
+def vm_print(vm_state: VmState, *args, op_bytecode=None, **kwargs) -> VmState:
+    """Read value from register or memory and print it to stdout."""
+    op_code, arg1_type, arg1, _, _ = op_bytecode
+
+    assert VM_OPERATION_TO_BYTECODE[op_code] == "PRINT"
+
+    if arg1_type == 2:  # Register
+        value_for_print = vm_state.vm_registers[arg1].value
+
+    elif arg1_type == 3:  # Register pointer
+        mem_address = vm_state.vm_registers[arg1].value
+        value_for_print = vm_state.vm_memory[mem_address]
+
+    else:
+        raise Exception("Bad print source")
+
+    print(f'VM PRINT: {value_for_print}')
+
+    return vm_state
